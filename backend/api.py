68a3c3f15cc48e37eb72090816e87fd275f6d08d
Padena Rasouli-Baghban <padenarasouli@gmail.com>
2022-08-09 22:58:00 -0400

    made frontend

--- backend/api.py
+++ backend/api.py
@@ -3,9 +3,11 @@ import joblib
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 import requests
+from flask_cors import CORS
 
 load_dotenv()
 app = Flask(__name__)
+CORS(app)
 
 #get authorization for spotify
 AUTH_URL = 'https://accounts.spotify.com/api/token'
@@ -38,21 +40,34 @@ def find_genre(id):
     song_features = get_song_info(id)
     #run ml model on song features:
     model = joblib.load('./model/genre-finder')
-    genre = model.predict(song_features)
-    genre = genre[0]
-    genres = []
+    genre_num = model.predict(song_features)
+    genre_num = genre_num[0]
+    genres = ["acoustic", "alternative", "blues", "bollywood", "country", "hip-hop", "indie", "instrumental", "metal", "pop", "rock"];
+    genre = genres[genre_num]
     return genre
 
 
 #get recommendations method
-def get_recommendations(id, genre):
-    #get recommendations based on spotify api
-    url = "https://api.spotify.com/v1/recommendations?seed_genres" + genre + "&seed_tracks" + id 
-    res = requests.get(url, headers=headers)
-    #TODO: figure out which part of the response i wanna keep
-    return res
+@app.route('/api/rec', methods=['GET'])
+def get_recommendations():
+    if 'id' in request.args:
+        id = request.args['id']
+    else:
+        return "Error: No id field provided. Please specify an id."
+    if 'genre' in request.args:
+        genre = request.args['genre']
+    else:
+        return "Error: No gnre field provided. Please specify a genre."
 
-@app.route('/api/songinfo', methods=['GET'])
+    #get recommendations based on spotify api:
+    url = "https://api.spotify.com/v1/recommendations?seed_genres=" + genre + "&seed_tracks=" + id 
+    res = requests.get(url, headers=headers)
+    res = res.json()
+    rec_list = res['tracks'][0]['id'];
+    print(rec_list)
+    return jsonify({"rec": str(rec_list)})
+
+@app.route('/api/genre', methods=['GET'])
 def get_songbyid():
     if 'id' in request.args:
         id = request.args['id']
@@ -60,6 +75,7 @@ def get_songbyid():
         return "Error: No id field provided. Please specify an id."
 
     genre = find_genre(id) # call function to find genre
+    print(genre);
     return jsonify({"genre": str(genre)})
 
 app.run()
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,49 @@
+console.log("hello");
 //send spotify id to backend
 //get recommendations based on genre
 //display results and recommendations
+
+//you are likely to enjoy this genre and these songs
+
+const genreAPI = async (songId) => {
+    const response = await fetch('http://localhost:5000/api/genre?id=' + songId);
+    let genreJSON = await response.json();
+    genre = genreJSON['genre'];
+    console.log(genre);
+    document.getElementById("genre-results").innerHTML = `
+            <div class="card is-pinker">
+            <div class="card-content">
+            <div class="content source-font has-text-centered">
+                <p>🎵You are likely to enjoy the following genre:</p>
+                <strong>✨${genre}<strong>
+            </div>
+            </div>
+        </div>
+    `;
+
+    const response2 = await fetch('http://127.0.0.1:5000/api/rec?id=' + songId + "&genre=" + genre);
+    let recJSON = await response2.json();
+    rec = recJSON.rec;
+    console.log(rec);
+    document.getElementById("rec-results").innerHTML = `
+        <div class="card is-pinker">
+            <div class="card-content">
+            <div class="content source-font has-text-centered">
+            <p>🎵You should give this song a try:</p>
+                <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/${rec}?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+            </div>
+            </div>
+        </div>
+    `;
+}
+document.getElementById('button').addEventListener('click', () => {
+    const input = document.getElementById('input').value;
+    if (input.substring(0,31) == "https://open.spotify.com/track/"){
+        const songId = input.substring(31,53);
+        console.log(songId);
+        genreAPI(songId);
+    } else {
+        alert("That's not a valid spotify link 🤨");
+        return;
+    }
+});
